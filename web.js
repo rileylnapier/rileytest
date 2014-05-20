74a3bf704d6ca7f1ea5b90ef6f0950a4d1122f72
Riley Napier <rileynapier@Rileys-MacBook-Air.local>
2014-05-20 15:53:41 -0500

    init

--- web.js
+++ web.js
@@ -6,26 +6,26 @@ var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 
 app.use(logfmt.requestLogger());
 
-app.get("/order/:order_id", function(req, res) {
-	console.log(req.params);
+app.get("/order/:order_id/:email", function(req, res) {
+
     var order_id = req.params.order_id;
-    //var email = req.params.email;
+    var email = req.params.email;
     var coupon = "ingressHint";
     var secret_code = 'not_set';
 
     //use order_id, email and coupon to verify order using symphony API
     if (true) {
     	redis.hmget("USED_CODES", order_id, function(err, value) {
-
+    		//console.log(value);
 			if (!value.toString()) {
 					redis.lpop('NEW_CODES', function(err2, value2) {
-						secret_code = value2.toString();
+						secret_code = value2;
 						redis.hset("USED_CODES", order_id, secret_code);
 						res.send(200, {"order_id": order_id, "secret_code": secret_code});
 					});
 				
 			} else {
-				secret_code = value.toString();
+				secret_code = value;
 				res.send(200, {"order_id": order_id, "secret_code": secret_code});
 			}
 		});
