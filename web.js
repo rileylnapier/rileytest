5cf4bcf1b02fefb5a59b0107d6d8a42b11d0c037
Riley Napier <rileynapier@Rileys-MacBook-Air.local>
2014-05-21 00:22:35 -0500

    init

--- web.js
+++ web.js
@@ -4,7 +4,21 @@ var logfmt = require("logfmt");
 var request = require("request");
 var app = express();
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(logfmt.requestLogger());
 
 app.get("/order/:order_id/:email", function(req, res) {
@@ -73,6 +87,8 @@ app.get("/order/:order_id/:email", function(req, res) {
     	}
 	});
 });
+
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
